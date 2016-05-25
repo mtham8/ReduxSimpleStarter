d19ad20a4028f5ff269541aca68c766715617768
Michelle <mtham101@gmail.com>
2016-05-24 19:45:02 -0700

    section 1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
+// react creates multiple components
+// a component is a js function that produces html
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyCdWcWxdO0zUOZP5Hd7cQkWhOrP3C8Usmw';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// create a new component. This component should produce some HTML
+// const app = function () {}
+const App = () => {
+  // const is ES6 syntax -- declares a variable with a final value -- not going to reassign app
+  return <div>Hello world!</div>;
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// take this component's generated html and put it on
+// the page (in the DOM)
+// when you're creating a component, you're creating a class of a component
+// you want to pass in an instance of a component in .render()
+// wrap your component in jsx tags when render
+ReactDOM.render(<App />, document.querySelector('.container'));
+// a component is a function/object that returns HTML
+
